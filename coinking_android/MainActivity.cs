f4a0a2e4aaba0f044e121abf26c3fc2579661244
teknogeek <jmargolis97@gmail.com>
2014-09-06 18:12:43 -0400

    first commit

---
+++ coinking_android/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace coinking_android
+{
+	[Activity (Label = "coinking_android",  MainLauncher = true)]			
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Create your application here
+
+			EditText apiKeyEditText = FindViewById<EditText>(Resource.Id.apiKey);
+			Button loadButton = FindViewById<Button>(Resource.Id.loadAPI);
+
+			string apiKey = string.Empty;
+				
+			loadButton.Click += (object sender, EventArgs e) => 
+			{
+				apiKey = apiKeyEditText.Text;
+
+				if(!String.IsNullOrWhiteSpace(apiKey))
+				{
+
+
+					List<string> apiInfo = coinking_android.apiParser.getApiInfo(apiKey);
+					var intent = new Intent(this, typeof(APIList));
+					intent.PutStringArrayListExtra("api_info", apiInfo);
+					StartActivity(intent);
+				}
+			};
+		}
+	}
+}
+
---
+++ coinking_android/apiParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Json;
+using System.IO;
+
+namespace coinking_android
+{
+	public static class apiParser
+	{
+		public static List<string> getApiInfo(string apiKey)
+		{
+			WebRequest req = HttpWebRequest.Create("https://coinking.io/api.php?key=" + apiKey + "&type=myinfo&output=json");
+
+			using(Stream responseStream = req.GetResponse().GetResponseStream())
+			{
+				JsonObject userInfo = (JsonObject)JsonObject.Load(responseStream);
+				string currentCoin = userInfo["currentCoinNickname"];
+
+				List<string> apiInfoList = new List<string>();
+
+				apiInfoList.Add(currentCoin);
+				return apiInfoList;
+			}
+		}
+	}
+}
+
